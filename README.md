b6f4e99dea2c08f694ae412be4a9f32f92d5c95f
Amber Jones <amberlovescats14@gmail.com>
2019-08-23 18:35:42 -0500

    working on routes with PostMan

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # MERN_CRUD_Practice
-I need to keep practicing my skills so I am going to build a full stack app in React.
+It has been 2 weeks since I made my last Full Stack App, so I am going to create one and use it as my portfolio.
+
+I am not going to use BootStrap to style this, I may choose between ANT Design, MaterialUI, or TailWinds
+
+Also this app will be public for employers, so there will be no authorization, I will rely on individuals to only edit and delete their own comments.
+
+This app will include, a server, a database, Schemas, routes, React and Heroku.
--- routes/comments.js
+++ routes/comments.js
@@ -12,5 +12,34 @@ router.get('/', async (req, res)=> {
   }
 })
 
+router.post('/', async (req, res)=> {
+  try {
+    const newComment = new Comments({
+      text: req.body.text
+    })
+    const save = await newComment.save()
+      res.json(save)
+  } catch (error) {
+    console.error(error.message)
+    res.status(500).json({msg: `SERVER ERROR`})
+  }
+})
+
+router.put('/:id', async (req, res) => {
+  try {
+    const singleComment = await Comments.findById(req.params.id)
+    if(!singleComment){
+      return res.status(404).json({msg: `NOT FOUND`})
+    } else {
+      singleComment.text = req.body.text
+      await singleComment.save()
+      return res.json(singleComment)
+    }
+    
+  } catch (error) {
+    console.error(error.message)
+    res.send(500).json({msg: `SERVER ERROR`})
+  }
+})
 
 module.exports = router
